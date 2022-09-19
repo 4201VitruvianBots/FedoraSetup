58a6a4a68bce483b82142f105fc62e59efa0c0df
JosiahFu <josiahfu@gmail.com>
2022-09-18 18:34:17 -0700

    Fixes

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-echo "This script is not ready for usage yet!"
-exit 2
+#echo "This script is not ready for usage yet!"
+#exit 2
+
+read -p "Hostname: " hostname
+hostnamectl hostname $hostname
 
 mkdir /tmp/work
 
@@ -17,31 +20,26 @@ cat etc/dnf/dnf.conf >> /etc/dnf/dnf.conf
 dnf upgrade
 flatpak update
 
-# TODO dconf
-
-read hostname
-hostnamectl hostname $hostname
+#sudo $USERNAME source ./dconf.sh
 
 dnf install -y vim
-# TODO RPM Fusion
-# TODO Flathub
+
+dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install -y com.visualstudio.code
 flatpak install -y com.jetbrains.IntelliJ-IDEA-Community
-flatpak install -y com.slack.com
+flatpak install -y com.slack.Slack
 flatpak install -y com.axosoft.gitkraken
-# TODO Chrome
-# TODO Media Codecs
+dnf install google-chrome-stable
+dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
+dnf install lame\* --exclude=lame-devel
+dnf group upgrade --with-optional Multimedia
 
 # TODO Firefox config
 
-# TODO Dark Theme
 
 dnf install -y gnome-tweaks
 
-# TODO GNOME Tweaks
-
 flatpak install -y com.mattjakeman.ExtensionManager
 
-# TODO GNOME Extensions
-
-cp .bashrc.d/* ~/.bashrc.d
+dnf install gnome-shell-extension-drive-menu
